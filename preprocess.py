0a45997758f21b5f5ad5fc851f32b20639f7136e
Jonathan Ve Vance <jonathanvevance@gmail.com>
2021-08-26 17:07:56 +0530

    more comments

--- preprocess.py
+++ preprocess.py
@@ -25,7 +25,7 @@ def molecule(mols, src_len, reactant_mask = None, ranges = None):
     features = {}
     element = np.zeros(src_len, dtype='int32')
     aroma = np.zeros(src_len, dtype='int32')
-    bonds = np.zeros((src_len, MAX_BONDS), dtype='int32')
+    bonds = np.zeros((src_len, MAX_BONDS), dtype='int32') #! Note... 2D array
     charge = np.zeros(src_len, dtype='int32')
 
     reactant = np.zeros(src_len, dtype='int32') # 1 for reactant
@@ -33,7 +33,7 @@ def molecule(mols, src_len, reactant_mask = None, ranges = None):
     segment = np.zeros(src_len, dtype='int32')
 
     for molid, mol in enumerate(mols):
-        for atom in mol.GetAtoms():
+        for atom in mol.GetAtoms(): #! Note.. each atom has a different idx
             idx = atom.GetAtomMapNum()-1
 
             segment[idx] = molid
@@ -42,25 +42,29 @@ def molecule(mols, src_len, reactant_mask = None, ranges = None):
             mask[idx] = 0
             if reactant_mask:
                 reactant[idx] = reactant_mask[molid]
+            #! getting features of atoms in mol
 
-            cnt = 0
+            cnt = 0 #! tracks the TOTAL number of bonds from this atom
             for j, b in enumerate(atom.GetBonds()): # mark existence of bond first
-                other = b.GetBeginAtomIdx() + b.GetEndAtomIdx() - atom.GetIdx()
-                other = mol.GetAtoms()[other].GetAtomMapNum() - 1
+
+                print("bond =", b)
+
+                other = b.GetBeginAtomIdx() + b.GetEndAtomIdx() - atom.GetIdx() #! get other atom Idx
+                other = mol.GetAtoms()[other].GetAtomMapNum() - 1 #! atomic number of other atom
                 num_map = {'SINGLE': 1, 'DOUBLE': 2, 'TRIPLE': 3, 'AROMATIC': 1}
-                num = num_map[str(b.GetBondType())]
+                num = num_map[str(b.GetBondType())] #! int number of bonds (or 1 for aromatic)
                 for k in range(num):
-                    if cnt == MAX_BONDS:
+                    if cnt == MAX_BONDS: #! if TOTAL no. of bonds > MAX_BONDS, ignore this reaction (return None)
                         return None
-                    bonds[idx][cnt] = other
+                    bonds[idx][cnt] = other #! IMPORTANT = for idx atom, bond number 'cnt' is with this atomic number...
                     cnt += 1
                 if str(b.GetBondType()) == 'AROMATIC':
                     aroma[idx] = 1
             tmp = bonds[idx][0:cnt]
             tmp.sort()
-            bonds[idx][0:cnt] = tmp
+            bonds[idx][0:cnt] = tmp #! sort the atomic numbers (for consistency because this is a graph)
             while cnt < MAX_BONDS:
-                bonds[idx][cnt] = idx
+                bonds[idx][cnt] = idx #! IMPORTANT = for others cnt values till MAX_BONDS, put lone pairs.
                 cnt += 1
 
     features = {'element':element, 'bond':bonds, 'charge':charge, 'aroma':aroma, 'mask':mask, 'segment':segment, 'reactant': reactant}
@@ -71,7 +75,7 @@ def reaction(args):
     """ processes a reaction, returns dict of arrays"""
     src, tgt = args
 
-    #! Alert
+    #! Note...
     print("\n\nSource is....")
     print(src)
     print("\n\nTarget is....")
@@ -81,20 +85,22 @@ def reaction(args):
     src_len = Chem.MolFromSmiles(src).GetNumAtoms()
     tgt_len = Chem.MolFromSmiles(tgt).GetNumAtoms() #! I added this... (tgt_len != src_len)
 
-    #! Alert
+    #! Note...
     print("\nSource length = ", src_len)
     print("Target length = ", tgt_len)
 
     # reactant mask
     src_mols = src.split('.')
 
-    #! Alert
+    #! Note...
     print("\n#src_mols = ", len(src_mols))
 
-    tgt_atoms = pattern.findall(tgt) #! Looking for SAME PATTERN AS LHS in RHS.. (look for datasets)
+    src_atoms = pattern.findall(src) #! I added this.. [this will be from 1 to some max number]
+    tgt_atoms = pattern.findall(tgt) #! RHS side SHOULD BE MAPPED..
 
-    #! Alert
-    print("\n#tgt_atoms = ", len(tgt_atoms))
+    #! Note...
+    print("#src_atoms = ", src_atoms)
+    print("#tgt_atoms = ", tgt_atoms) #! len(tgt_atoms) = tgt_len
 
     reactant_mask = [False for i in src_mols]
     for j, item in enumerate(src_mols):
@@ -104,9 +110,17 @@ def reaction(args):
                 reactant_mask[j] = True
                 break
 
+    #! Note...
+    print("\nreactant_mask = ", reactant_mask) #! reactants that take part in rxn
+
     # the atom map num ranges of each molecule for segment mask
     src_mols = [Chem.MolFromSmiles(item) for item in src_mols]
     tgt_mols = [Chem.MolFromSmiles(item) for item in tgt.split(".")]
+
+    #! Note...
+    print("\n#src_mols = ", len(src_mols))
+    print("#tgt_mols = ", len(tgt_mols))
+
     ranges = []
     for mol in src_mols:
         lower = 999
@@ -114,21 +128,34 @@ def reaction(args):
         for atom in mol.GetAtoms():
             lower = min(lower, atom.GetAtomMapNum()-1)
             upper = max(upper, atom.GetAtomMapNum())
-        ranges.append((lower, upper))
+        ranges.append((lower, upper)) #! For each src molecule, get (min_mapno-1, max_mapno)
+        #! However, ranges is not used anywhere.... [even in molecule()]
 
     src_features = molecule(src_mols, src_len, reactant_mask, ranges)
-    tgt_features = molecule(tgt_mols, src_len)
+    tgt_features = molecule(tgt_mols, src_len) #! length that is passed is src_len
 
+    #! Note...
+    print("\n#src_features = ", len(src_features))
+    print("#tgt_features = ", len(tgt_features)) #! both are of same length
+
+    #! Note...
+    print("\n#src_features.element = ", src_features["element"])
+    print("#tgt_features.element = ", tgt_features["element"])
+
+    # #! Note...
+    # print("\n#src_features.bond = ", src_features["bond"])
+    # print("#tgt_features.bond = ", tgt_features["bond"])
 
     if not (src_features and tgt_features):
         return None
 
+    #! This section is just to ignore any rxn with diff > MAX_DIFF bond changes (verify wording)
     src_bond = src_features['bond']
     tgt_bond = tgt_features['bond']
     bond_inc = np.zeros((src_len, MAX_DIFF), dtype='int32')
     bond_dec = np.zeros((src_len, MAX_DIFF), dtype='int32')
     for i in range(src_len):
-        if tgt_features['mask'][i]:
+        if tgt_features['mask'][i]: # 1 for masked
             continue
         inc_cnt = 0
         dec_cnt = 0
@@ -155,7 +182,6 @@ def reaction(args):
             item['src_' + key] = src_features[key]
             item['tgt_' + key] = tgt_features[key]
 
-    exit() #! Alert
     return item
 
 
@@ -172,7 +198,7 @@ def process(name):
             src.append(rxn[0])
             tgt.append(rxn[1])
 
-            break # only one reaction #! ALert
+            break # only one reaction #! Note..
 
     pool = ProcessPoolExecutor(10)
     dataset = []
